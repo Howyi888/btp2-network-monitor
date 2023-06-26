f3c3d24156f38642296d81df5281d4d59b106205
MoonKyu Song <mksong@parametacorp.com>
2023-06-26 17:44:20 +0900

    Support "noweb" argument in run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,10 @@ export NETWORKS_JSON="networks.json"
 export DOCUEMNT_ROOT="web/build"
 export STORAGE_URL="storage.db"
 
-( cd web ; npm run start & )
-NPM_PID=$!
-trap "kill ${NPM_PID}" EXIT
+if [ "$1" != "noweb" ] ; then
+    ( cd web ; npm run start & )
+    NPM_PID=$!
+    trap "kill ${NPM_PID}" EXIT
+fi
 
 exec uvicorn btp2_monitor.webui:app --reload
